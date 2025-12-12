6ba0d8e54aef91950058872565802b76ca1fb1e2
AmulyaVudutha <amulyavudutha07@gmail.com>
2025-12-12 20:37:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (43.47%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long power = n;   
+        double result = 1.0;
+        if (power < 0) {
+             x = 1 / x;
+            power = -power;
+        }
+        while (power > 0) {
+            if (power & 1) {
+                result *= x;
+            }
+
+            x *= x;        
+            power >>= 1; 
+        }
+
+
+        return result;
+    }
+};
